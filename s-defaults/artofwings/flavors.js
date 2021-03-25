79183a935472795b35dfbd4bb930856d63d42aee
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-03-25 17:29:51 -0400

    Improved

---
+++ s-defaults/artofwings/flavors.js
@@ -0,0 +1,65 @@
+// [EXPORT] //
+module.exports = {
+	flavors: [
+		{
+			title: 'Plain',
+			description: '',
+		},
+		{
+			title: 'Pineapple BBQ',
+			description: '',
+		},
+		{
+			title: 'Honey BBQ',
+			description: '',
+		},
+		{
+			title: 'Honey Garlic',
+			description: '',
+		},
+		{
+			title: 'Asian Sesame',
+			description: '',
+		},
+		{
+			title: 'General Tso',
+			description: '',
+		},
+		{
+			title: 'Mojita Lime',
+			description: '',
+		},
+		{
+			title: 'Buffalo Dry Rub',
+			description: '',
+		},
+		{
+			title: 'Buffalo Mild',
+			description: '',
+		},
+		{
+			title: 'Teriyaki Sauce',
+			description: '',
+		},
+		{
+			title: 'Lousiana Sauce',
+			description: '',
+		},
+		{
+			title: 'Korean BBQ',
+			description: '',
+		},
+		{
+			title: 'Ragin\' Cajun',
+			description: '',
+		},
+		{
+			title: 'Mango Habanero',
+			description: '',
+		},
+		{
+			title: 'Knockout',
+			description: '',
+		},
+	],
+}
