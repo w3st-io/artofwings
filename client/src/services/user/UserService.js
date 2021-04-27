5c4487061716bfb4b1f0c6c608d690f4fb230634
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-04-27 13:46:08 -0400

    s

--- client/src/services/user/UserService.js
+++ client/src/services/user/UserService.js
@@ -143,7 +143,7 @@ async function s_resendVerificationEmail(email) {
 
 
 /******************* [PASSWORD] *******************/
-async function s_resetPassword(currentPassword, password) {
+async function s_changePassword(currentPassword, password) {
 	try {
 		const authAxios = await this.authAxios()
 		
@@ -224,7 +224,7 @@ export default {
 	s_logout,
 	s_register,
 	s_verify,
-	s_resetPassword,
+	s_changePassword,
 	s_resendVerificationEmail,
 	s_requestResetPassword,
 	s_notLoggedResetPassword,
