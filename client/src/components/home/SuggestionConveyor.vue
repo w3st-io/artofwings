7199b54266ea8fcfb689d40fabb8362ccc23324e
aleem-ahmed <opall.io2020@gmail.com>
2021-05-13 22:01:58 -0400

    Improved

--- client/src/components/home/SuggestionConveyor.vue
+++ client/src/components/home/SuggestionConveyor.vue
@@ -3,25 +3,53 @@
 			<div class="py-4">
 				<BContainer class="my-3 text-center">
 					<BCard bg-variant="none" class="shadow">
-						<!-- Title -->
-						<div class="mt-2 mb-4">
-							<span class="h1 px-2 text-center text-light bg-primary">
-								Combos <span class="text-secondary">&</span> Deals
-							</span>
-						</div>
-
 						<BRow>
-							<BCol cols="1" class="d-none d-md-block m-0 p-0">
-								<BButton
-									variant="primary"
-									id="prevButton"
-									class="mt-5 px-0 w-100 shadow"
-									style="max-width: 64px;"
-								><ArrowLeftIcon /></BButton>
+							<!-- !XL Title -->
+							<BCol cols="12" class="d-block d-xl-none mb-3">
+								<div class="">
+									<span class="h3 px-2 text-center text-light bg-primary">
+										Combos <span class="text-secondary">&</span> Deals
+									</span>
+								</div>
 							</BCol>
 
+							<!-- Left Button -->
+							<BCol cols="6" xl="4" class="mb-3 mb-xl-5">
+								<BButton
+									variant="outline-light"
+									id="prevButton"
+									class="px-0 w-100 text-dark"
+								>
+									<ArrowLeftIcon size="3x" class="d-none d-xl-block m-auto" />
+									<ArrowLeftIcon size="2.5x" class="d-block d-xl-none m-auto" />
+								</BButton>
+							</BCol>
+
+							<!-- XL Title -->
+							<BCol cols="4" class="d-none d-xl-block mb-3 mb-xl-5">
+								<div class="mt-2 rounded">
+									<span class="h1 px-2 text-center text-light bg-primary shadow">
+										Combos <span class="text-secondary">&</span> Deals
+									</span>
+								</div>
+							</BCol>
+
+							<!-- Right Button -->
+							<BCol cols="6" xl="4" class="mb-3 mb-xl-5">
+								<BButton
+									variant="outline-light"
+									id="nextButton"
+									class="px-0 w-100 text-dark"
+								>
+									<ArrowRightIcon size="3x" class="d-none d-xl-block m-auto" />
+									<ArrowRightIcon size="2.5x" class="d-block d-xl-none m-auto" />
+								</BButton>
+							</BCol>
+						</BRow>
+
+						<BRow>
 							<!-- All Sliders -->
-							<BCol cols="12" md="10" class="my-slider">
+							<BCol cols="12" class="my-slider">
 								<!-- Tiny Slider -->
 								<VueTinySlider ref="slider" v-bind="options">
 									<div
@@ -58,15 +86,6 @@
 										</a>
 									</div>
 								</VueTinySlider>
-							</BCol>
-
-							<BCol cols="1" class="d-none d-md-block m-0 p-0">
-								<BButton
-									variant="primary"
-									id="nextButton"
-									class="mt-5 px-0 w-100 shadow"
-									style="max-width: 64px;"
-								><ArrowRightIcon /></BButton>
 							</BCol>
 						</BRow>
 					</BCard>
