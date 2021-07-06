8b59f09d7e59a9e81308e0edd1a4d04554babb5b
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-07-06 03:00:32 -0400

    Improved

--- s-routes/pages/index.js
+++ s-routes/pages/index.js
@@ -21,51 +21,56 @@ router.get(
 	async (req, res) => {
 		try {
 			// [INIT] //
-			let customHome = false
 			const user_id = (req.user_decoded) ? req.user_decoded.user_id : undefined
 
 			// Set Custom Home Status //
-			if (config.CUSTOM_HOME == 'true') { customHome = true }
-
-			for (let i = 0; i < cats.length; i++) {
-				// [FILL][TOTAL-POSTS] //
-				cats[i].totalPosts = (
-					await postsCollection.c_countByCat(cats[i].cat_id)
-				).count
-
-				// [FILL][RECENT-POST] //
-				cats[i].recentPost = (
-					await postsCollection.c_readByCatSorted(
-						user_id,
-						cats[i].cat_id,
-						0,
-						1,
-						0
-					)
-				).posts[0]
-
+			if (config.CUSTOM_HOME == 'true') {
+				res.status(200).send({
+					executed: true,
+					status: true,
+					customHome: true
+				})
 			}
-			
-			// [TOP-POSTS] //
-			const topPosts = (
-				await postsCollection.c_readSorted(user_id, 1, 5, 0)
-			).posts
+			else {
+				for (let i = 0; i < cats.length; i++) {
+					// [FILL][TOTAL-POSTS] //
+					cats[i].totalPosts = (
+						await postsCollection.c_countByCat(cats[i].cat_id)
+					).count
+	
+					// [FILL][RECENT-POST] //
+					cats[i].recentPost = (
+						await postsCollection.c_readByCatSorted(
+							user_id,
+							cats[i].cat_id,
+							0,
+							1,
+							0
+						)
+					).posts[0]
+	
+				}
+				
+				// [TOP-POSTS] //
+				const topPosts = (
+					await postsCollection.c_readSorted(user_id, 1, 5, 0)
+				).posts
 
-			
-			res.send({
-				executed: true,
-				status: true,
-				customHome: customHome,
-				cats: cats,
-				topPosts: topPosts,
-				cryptoQuote: cryptoQuote.prices,
-			})
+				res.send({
+					executed: true,
+					status: true,
+					customHome: false,
+					cats: cats,
+					topPosts: topPosts,
+					cryptoQuote: cryptoQuote.prices,
+				})
+			}
 		}
 		catch (err) {
 			res.send({
 				executed: false,
 				status: false,
-				message: `/pages: Error --> ${err}`
+				message: `/pages/: Error --> ${err}`
 			})
 		}
 	}
